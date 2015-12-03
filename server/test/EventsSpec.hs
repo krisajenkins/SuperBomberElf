890d539027918e91beaa2444b9845c90ec38abac
Kris Jenkins <krisajenkins@gmail.com>
2015-12-03 21:20:29 +0000

    Tidying the event tests.

--- server/test/EventsSpec.hs
+++ server/test/EventsSpec.hs
@@ -20,7 +20,8 @@ instance Arbitrary Event where
           ,RemovePlayer <$> arbitrary]
 
 spec :: Spec
-spec = scheduleSpec
+spec = do scheduleSpec
+          eventSpec
 
 eventsCause
   :: [(Event,Time)] -> Time -> (Scene,[Reaction]) -> Expectation
@@ -31,39 +32,38 @@ eventsCause events time expected =
 scheduleSpec :: Spec
 scheduleSpec =
   describe "Schedule Handling" $
-  do it "Join and name should result in one named player" $
-       property $
+  it "Future events are not processed." . property $
+  \(t,(e1,e2)) ->
+    (sceneAt (scheduleAt Map.empty
+                         [(e1,t)])
+             t) `shouldBe`
+    (sceneAt (scheduleAt Map.empty
+                         [(e1,t),(e2,addTime 5 t)])
+             t)
+
+eventSpec :: Spec
+eventSpec =
+  describe "Event Processing" $
+  do it "Join and name should result in one named player" . property $
        \(t,uuid,name) ->
          eventsCause [(AddPlayer uuid,t),(SetPlayerName uuid name,addTime 5 t)]
                      (addTime 10 t)
                      (Scene {_players = Map.fromList [(uuid,Player name)]},[])
-     it "Removing a non-existent player shouldn't matter." $
-       property $
+     it "Removing a non-existent player shouldn't matter." . property $
        \(t,uuid) ->
          eventsCause [(RemovePlayer uuid,t)]
                      (addTime 10 t)
                      (Scene Map.empty,[])
-     it "Adding n players results in n players." $
-       property $
+     it "Adding n players results in n players." . property $
        \((NonEmpty srcs) :: NonEmptyList (UUID,Time)) ->
          let events = fmap (\(uuid,t) -> (AddPlayer uuid,t)) srcs
              time = maximum (snd <$> srcs)
              (scene,_) = sceneAt (scheduleAt Map.empty events) time
          in length (_players scene) == length srcs
-     it "Double adding a player causes an error." $
-       property $
+     it "Double adding a player causes an error." . property $
        \(t1,t2,uuid) ->
          eventsCause
            [(AddPlayer uuid,t1),(AddPlayer uuid,t2)]
            (max t1 t2)
            (Scene {_players = Map.fromList [(uuid,Player Nothing)]}
            ,[PlayerAlreadyAdded uuid])
-     it "Future events are not processed." $
-       property $
-       \(t,(e1,e2)) ->
-         (sceneAt (scheduleAt Map.empty
-                              [(e1,t)])
-                  t) `shouldBe`
-         (sceneAt (scheduleAt Map.empty
-                              [(e1,t),(e2,addTime 5 t)])
-                  t)
